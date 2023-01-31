96ae718b25103e4b4fbe28d2a349b21df1c9966c
Hasan Oztunc <hasanhuseyin9e@gmail.com>
2023-01-31 20:55:07 +0300

    Revert "commit test"
    This reverts commit 7ebf622386780258d98f3c27cbf09915d84f442d.

--- TFCase/Scenes/Contents/ViewControllers/ContentsViewController.swift
+++ TFCase/Scenes/Contents/ViewControllers/ContentsViewController.swift
@@ -14,7 +14,7 @@ final class ContentsViewController: TFViewController<ContentsViewModel> {
     @IBOutlet private weak var searchTextField: UITextField!
     @IBOutlet private weak var contentsCollectionView: UICollectionView!
     
-    // MARK: - Private
+    // MARK: - Privates
     
     private var contents: [[ContentCollectionViewCellViewModel]] = []
     private var isPageStillRefreshing: Bool = false
